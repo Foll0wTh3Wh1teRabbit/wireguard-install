a98c1bb9be638f70cc79f9a49d2c505f965501a8
Foll0wTh3Wh1teRabbit <n.kosarev@g.nsu.ry>
2025-07-28 11:58:10 +0700

    Fix pattern revoke

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -492,10 +492,10 @@ function revokeClientsByPattern() {
 
   readarray -t CLIENT_NAMES < <(grep -E "^### Client" "/etc/wireguard/${SERVER_WG_NIC}.conf" | grep "$1")
   for name in "${CLIENT_NAMES[@]}"; do
-    	sed -i "/^### Client $name\$/,/^$/d" "/etc/wireguard/${SERVER_WG_NIC}.conf"
+    	sed -i "/^### Client ${name}\$/,/^$/d" "/etc/wireguard/${SERVER_WG_NIC}.conf"
 
-    	HOME_DIR=$(getHomeDirForClient "$name")
-    	rm -f "${HOME_DIR}/$name.conf"
+    	HOME_DIR=$(getHomeDirForClient "${name}")
+    	rm -f "${HOME_DIR}/${name}.conf"
   done
 
   wg syncconf "${SERVER_WG_NIC}" <(wg-quick strip "${SERVER_WG_NIC}")
